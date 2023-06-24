ab8816e144ab3ab0804d2afd2cc5280336107e43
Anthonycodes101 <anthonyuzonna341@gmail.com>
2023-06-24 16:40:20 -0700

    added necessary article stuffs/blocs

--- lib/Presentation/Bloc/Article_list_bloc/article_list_bloc.dart
+++ lib/Presentation/Bloc/Article_list_bloc/article_list_bloc.dart
@@ -1,4 +1,9 @@
 import 'package:bloc/bloc.dart';
+import 'package:equatable/equatable.dart';
 
-//part 'article_top_headline_list_bloc.dart';
-// part 'article_list_event.dart';
+import '../../../Domain/Usecase/get_top_headline_articles.dart';
+import '../../../Domain/entities/article.dart';
+
+part 'article_top_headline_list_bloc.dart';
+part 'article_list_event.dart';
+part 'article_list_state.dart';
