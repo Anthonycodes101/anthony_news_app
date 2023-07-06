f52c6001f3b38251580414464827426c098cea0b
Anthonycodes101 <anthonyuzonna341@gmail.com>
2023-07-06 10:42:00 -0700

    added bloc provider to main

--- lib/Presentation/Bloc/Article_list_bloc/article_list_bloc.dart
+++ lib/Presentation/Bloc/Article_list_bloc/article_list_bloc.dart
@@ -1,9 +1,11 @@
 import 'package:bloc/bloc.dart';
 import 'package:equatable/equatable.dart';
 
+import '../../../Domain/Usecase/get_headline_business_articles.dart';
 import '../../../Domain/Usecase/get_top_headline_articles.dart';
 import '../../../Domain/entities/article.dart';
 
 part 'article_top_headline_list_bloc.dart';
 part 'article_list_event.dart';
 part 'article_list_state.dart';
+part 'article_headline_business_list_bloc.dart';
--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,18 @@
+import 'package:anthony_news_apps/Presentation/Bloc/Article_list_bloc/article_list_bloc.dart';
 import 'package:flutter/material.dart';
-import 'package:anthony_news_apps/injection.dart' as di ;
-void main() {
+import 'package:anthony_news_apps/injection.dart' as di;
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:provider/provider.dart';
+
+import 'Common/http_sslpinning.dart';
+import 'Presentation/Bloc/Article_details_bloc/article_detail_bloc.dart';
+import 'Presentation/Bloc/article_category_bloc/article_category_bloc.dart';
+import 'Presentation/Bloc/bookmark_article_bloc/bookmark_article_bloc.dart';
+import 'Presentation/Bloc/search_article_bloc/search_article_bloc.dart';
+
+void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await HttpSSLPinning.init();
   di.init();
   runApp(const MyApp());
 }
@@ -12,6 +23,28 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return MultiProvider(
+      providers: [
+        BlocProvider(
+          create: (_) => di.locator<ArticleTopHeadlineListBloc>(),
+        ),
+        BlocProvider(
+          create: (_) => di.locator<SearchArticleBloc>(),
+        ),
+        BlocProvider(
+          create: (_) => di.locator<ArticleCategoryBloc>(),
+        ),
+        BlocProvider(
+          create: (_) => di.locator<ArticleHeadlineBusinessListBloc>(),
+        ),
+        BlocProvider(
+          create: (_) => di.locator<ArticleDetailBloc>(),
+        ),
+
+        BlocProvider(
+          create: (_) => di.locator<BookmarkArticleBloc>(),
+        ),
+      ],
+    );
   }
 }
